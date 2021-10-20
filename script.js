88974176d968f75bc4b4eebefd02d2c08e33c1f5
Maja <maja.pavlovic.de@gmail.com>
2021-10-20 13:22:03 +0100

    fix site-nav (hamburger)

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ trigger.addEventListener('click', function(e) {
     const element = document.getElementById('menu-links');
 
     if (isMenuOpen) {
-        element.style.display = 'block';
+        element.classList.remove('hide');
+        // element.style.display = 'block';
     } else {
-        element.style.display = 'none';
+        element.classList.add('hide');
+        // element.style.display = 'none';
     }
 });
 
@@ -19,6 +21,7 @@ window.addEventListener('click', function(e) {
         isMenuOpen = false;
 
         const element = document.getElementById('menu-links');
-        element.style.display = 'none';
+        element.classList.add('hide');
+        // element.style.display = 'none';
     }
 });
